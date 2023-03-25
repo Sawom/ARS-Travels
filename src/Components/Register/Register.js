2ddcc784ba023b37ac2f6b5fab452d579275f0d6
Sawom <asawom250@gmail.com>
2023-03-25 17:50:23 +0600

    registere

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,21 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { getAuth, createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
 
 const Register = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [ confirmpass, setConfirmpass] = useState('');
+    const [ error, setError] = useState('');
+
+    const auth = getAuth();
+    // name
+    const handleName = event =>{
+        setName(event.target.value);
+    }
+    // email
+    const handleEmail = event =>{
+        setEmail(event.target.value);
+    }
+    // password
+    const handlePassword = event =>{
+        setPassword(event.target.value);
+    }
+    // confirm password
+    const handleConfirmpass = event =>{
+        setConfirmpass(event.target.value);
+    }
+    // error
+    const handleError = event=>{
+        setError(event.target.value);
+    }
+    // verify email
+    const verifyEmail = () =>{
+        sendEmailVerification(auth.currentUser)
+        .then((result)=>{
+            console.log(result);
+        })
+    }
+    // register new user
+    const registerNewUser = (email,password) =>{
+        createUserWithEmailAndPassword(auth, email , password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+            setError('');
+            verifyEmail();
+            setUserName();
+        })
+        .catch(error => {
+        setError(error.message);
+      })
+    }
+    // set user name
+    const setUserName = () => {
+        const auth = getAuth();
+        updateProfile(auth.currentUser, { displayName: name })
+        .then(result => {  
+            console.log(result);
+         })
+    }
+    
+    // create user
+    const handleRegistration = event =>{
+        event.preventDefault();
+         if(password !== confirmpass){
+            setError("Your password did not match! ");
+            return;
+        }
+        if(password.length < 6){
+            setError('Password Must be at least 6 characters long.');
+            return;
+        }
+        if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+            setError('Password Must contain 2 upper case');
+            return;
+        }
+        registerNewUser(email, password);
+    }
+
+
     return (
         <div>
             <h4 className='App my-4'>Register Here</h4>
             <div className='container my-4 center' >
-                <Form className='my-4' >
+                <Form className='my-4' onSubmit={handleRegistration} >
                     {/* name */}
                     <Form.Group className="mb-3" controlId="Name">
                         <Form.Label>Name</Form.Label>
-                        <Form.Control type="text" placeholder="Name" />
+                        <Form.Control type="text" onBlur={setUserName} placeholder="Name" />
                     </Form.Group>
                     {/* email */}
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control type="email" onBlur={handleEmail} placeholder="Enter email" required />
                         <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                         </Form.Text>
@@ -23,16 +100,16 @@ const Register = () => {
                     {/* password */}
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" onBlur={handlePassword} placeholder="Password" required />
                     </Form.Group>
                     {/*  */}
                     <Form.Group className="mb-3" controlId="formConfirmPassword">
                         <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control type="password" placeholder="Confirm Password" />
+                        <Form.Control type="password" onBlur={handleConfirmpass} placeholder="Confirm Password" required />
                     </Form.Group>
 
                     <Button variant="primary" type="submit">
-                        Register
+                         Register
                     </Button>
                 </Form>
             </div>
